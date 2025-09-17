6694f21e3f7b934ac871103281fa3703485a6b83
Isabela Wisses <wissesisabela@gmail.com>
2025-09-16 21:01:53 -0300

    Add files via upload

---
+++ Atividade02Isabela.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade02.isabela;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade02Isabela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
